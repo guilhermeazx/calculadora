51d8293986875aa02ed34283b4ce29cae7bed0ab
Guilherme <guiassishg11@gmail.com>
2023-05-29 20:33:19 -0300

    calculadora python

---
+++ calculadora.py
@@ -0,0 +1,30 @@
+def adicao ()
+    a = int(input(digite o primiro numero))
+    b = int(input(digite o segundo numero))
+    print(a + b)
+
+def subtracao ()
+    a = int(input(digite o primiro numero))
+    b = int(input(digite o segundo numero))
+    print(a - b)
+
+def multipicacao ()
+    a = int(input(digite o primiro numero))
+    b = int(input(digite o segundo numero))
+    print(a * b)
+
+def divisao() 
+    a = int(input(digite o primiro numero))
+    b = int(input(digite o segundo numero))
+    print(a / b)
+
+opcao = 1
+
+   while opcao
+    print('0 = sair')
+    print('1= adição')
+    print('2= subtração')
+    print('3 = multiplicação')
+    print('4 = divisão')
+
+    opcao = int(input('opção '))
